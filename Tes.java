41f276bb4ccaa48ed095e278d171551c02483bd7
ibadhamizan31 <ibad_hamizan_33rpl@student.smktelkom-mlg.sch.id>
2024-08-05 14:24:53 +0700

    Add files via upload

---
+++ Tes.java
@@ -0,0 +1,9 @@
+public class Tes {
+    public static void main(String[] args) {
+        String nama ="Ibad";
+        String kelas ="x RPL 5";
+
+        System.out.println("Nama saya " + nama + " dari kelas " + kelas);
+        
+    }
+}
